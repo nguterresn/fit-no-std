0b54978a72d495cf1c104a89989d8e8452c1f897
Nuno Guterres Nogueira <guterresnogueira@gmail.com>
2025-09-28 13:02:28 +0200

    more detailed workout example.

--- examples/workout.rs
+++ examples/workout.rs
@@ -47,7 +47,7 @@ fn main() {
     // Push sport, num_valid_steps
     let mut data: [u8; 3] = [0; 3];
     data[0] = FitSportType::Basketball as u8;
-    data[1..3].copy_from_slice(&(1u16).to_le_bytes()); // 1 step?
+    data[1..3].copy_from_slice(&(2u16).to_le_bytes()); // 2 steps?
     fit.push(&data).unwrap();
 
     // Define message_index, duration_type, duration_value, target_type
@@ -78,13 +78,20 @@ fn main() {
     )
     .unwrap();
 
+    // 2 Workout Steps
     let mut data: [u8; 7] = [0; 7];
     data[0] = 0;
     data[1] = FitWorkoutStepDuration::Time as u8;
-    data[2..6].copy_from_slice(&(1u32).to_le_bytes()); // 1 step? // Scale is 1000, 1 * 1000?
+    data[2..6].copy_from_slice(&(1u32).to_le_bytes()); // Scale is 1000, 1 * 1000?
     data[6] = FitWorkoutStepTarget::Speed as u8;
-
     fit.push(&data).unwrap();
+
+    data[0] = 1;
+    data[1] = FitWorkoutStepDuration::Time as u8;
+    data[2..6].copy_from_slice(&(2u32).to_le_bytes()); // Scale is 1000, 2 * 1000?
+    data[6] = FitWorkoutStepTarget::Speed as u8;
+    fit.push(&data).unwrap();
+
     let buf = fit.done().unwrap();
     // println!("{:02X?}", buf);
 
